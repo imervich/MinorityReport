8f0953f0de53eff43da493f44cb23b4a5f8bd353
Iván Mervich <imervich@gmail.com>
2014-07-24 10:25:43 -0500

    Initial Commit

---
+++ MinorityReport/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MinorityReport
+//
+//  Created by Iván Mervich on 7/24/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MinorityReport/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MinorityReport
+//
+//  Created by Iván Mervich on 7/24/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
